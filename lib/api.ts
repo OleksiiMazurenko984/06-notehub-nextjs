464669a8f15fc3ea7dc58512f987bfd0ff45dfd1
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-03-24 16:10:11 +0200

    Make homework tasks

---
+++ lib/api.ts
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import type { Note, NoteData, NoteDetails } from '@/types/note';
+
+const myKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+
+const notesInstance = axios.create({
+  baseURL: 'https://notehub-public.goit.study/api',
+  headers: {
+    Authorization: `Bearer ${myKey}`,
+  },
+});
+
+interface NotesResponse {
+  notes: Note[];
+  totalPages: number;
+}
+
+export const fetchNotes = async (
+  page: number,
+  perPage: number,
+  search?: string
+): Promise<NotesResponse> => {
+  const { data } = await notesInstance.get<NotesResponse>('/notes', {
+    params: {
+      page,
+      perPage,
+      search,
+    },
+  });
+
+  return data;
+};
+
+export const createNote = async (noteData: NoteData) => {
+  const { data } = await notesInstance.post<Note>('/notes', noteData);
+  return data;
+};
+
+export const deleteNote = async (noteId: Note['id']) => {
+  const { data } = await notesInstance.delete<Note>(`/notes/${noteId}`);
+  return data;
+};
+
+export const fetchNoteById = async (noteId: NoteDetails['id']) => {
+  const { data } = await notesInstance.get<NoteDetails>(`/notes/${noteId}`);
+  return data;
+};
---
+++ types/note.ts
@@ -0,0 +1,23 @@
+export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+
+export interface Note {
+  id: string;
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+export interface NoteData {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+export interface NoteDetails {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag: NoteTag;
+}
